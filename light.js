5d7faed3892365c67990559b8d89f6a3f30480f6
Vlad Harahan <vladyslavh@wix.com>
2019-04-28 21:40:16 +0300

    fix

--- light.js
+++ light.js
@@ -12,16 +12,16 @@ let source = "player"; // 'demo'
 let demoFrame = Array(64); // 'demo'
 
 function run() {
-  // dmx.connect();
+  dmx.connect();
 
   setInterval(() => {
     const t = ts.getAudioPlaybackDuration();
     const bytes = source === "demo" ? new Uint8Array(demoFrame) : player.getFrame(t);
 
     // console.log('bytes', bytes);
-    // dmx.setChannels(bytes, 0).catch(e => {
-    //   console.log("ERR", e);
-    // });
+    dmx.setChannels(bytes, 0).catch(e => {
+      console.log("ERR", e);
+    });
 
     emit(new Uint8Array(bytes));
   }, 30);
